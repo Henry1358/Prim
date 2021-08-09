41de432c02f34e9d6b935f3c7c9ed41e44175ecd
Henry1358 <88657307+Henry1358@users.noreply.github.com>
2021-08-09 15:37:39 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
+  appId: 'co.za.Prim.WinelandsGrocery',
   appName: 'Website to App',
   webDir: 'www',
   bundledWebRuntime: false,
